9d6e1657f53c866289b62e85e795884a92a49dce
Emanuel Merayo <emanuelmerayo@hotmail.com>
2018-04-06 00:04:43 -0300

    DRYing spec

--- spec/services/file_parser_service_spec.rb
+++ spec/services/file_parser_service_spec.rb
@@ -1,27 +1,32 @@
 require 'rails_helper'
 
 describe FileParserService, type: :class do
+  let(:file) { File.join(Rails.root, '/spec/fixtures/dados.txt') }
+
   describe 'call method' do
-    it 'must return 4 rows without the header' do
-      file = File.join(Rails.root, '/spec/fixtures/dados.txt')
-      parsed_rows = FileParserService.call(file, false)
+    context 'without header' do
+      let(:parsed_rows) { FileParserService.call(file, false) }
 
-      expect(parsed_rows.size).to eq 4
+      it 'must return 4 rows without the header' do
+        expect(parsed_rows.size).to eq 4
+      end
+
+      it 'must return an array' do
+        expect(parsed_rows).to be_instance_of(Array)
+      end
     end
 
-    it 'must return 5 rows including the header' do
-      file = File.join(Rails.root, '/spec/fixtures/dados.txt')
-      parsed_rows = FileParserService.call(file, true)
+    context 'with header' do
+      let(:parsed_rows) { FileParserService.call(file, true) }
 
-      expect(parsed_rows).not_to be_empty
-      expect(parsed_rows.size).to eq 5
-    end
+      it 'must return 5 rows including the header' do
+        expect(parsed_rows).not_to be_empty
+        expect(parsed_rows.size).to eq 5
+      end
 
-    it 'must return an array' do
-      file = File.join(Rails.root, '/spec/fixtures/dados.txt')
-      parsed_rows = FileParserService.call(file, true)
-
-      expect(parsed_rows).to be_instance_of(Array)
+      it 'must return an array' do
+        expect(parsed_rows).to be_instance_of(Array)
+      end
     end
   end
 end

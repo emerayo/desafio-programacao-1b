7c937ed681c16c38a27167dbdd645fb7fc587041
Emanuel Merayo <emanuelmerayo@hotmail.com>
2018-04-05 23:46:31 -0300

    DRYing spec

--- spec/controllers/sales_controller_spec.rb
+++ spec/controllers/sales_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SalesController, type: :controller do
+
+  let(:file) { fixture_file_upload('dados.txt', 'text/txt') }
+  let(:invalid_file) { fixture_file_upload('dados_invalidos.txt', 'text/txt') }
+
   describe 'GET index' do
     it 'must show sales ' do
       sale = FactoryBot.create(:sale)
@@ -15,36 +19,32 @@ RSpec.describe SalesController, type: :controller do
   end
 
   describe 'POST create' do
-    it 'should import the sales correctly' do
-      file = fixture_file_upload('dados.txt', 'text/txt')
+    context 'valid file' do
+      before do
+        post :create, params: { file: file }
+      end
 
-      post :create, params: { file: file }
+      it 'should import the sales correctly' do
+        expect(flash[:notice]).to be_eql('As vendas foram importadas com sucesso! O total é de: R$ 30,00')
+      end
 
-      expect(flash[:notice]).to be_eql('As vendas foram importadas com sucesso! O total é de: R$ 30,00')
+      it 'should increase the count of Sales' do
+        expect(Sale.count).to eq 4
+      end
     end
 
-    it 'should increase the count of Sales' do
-      file = fixture_file_upload('dados.txt', 'text/txt')
+    context 'invalid file' do
+      before do
+        post :create, params: { file: invalid_file }
+      end
 
-      post :create, params: { file: file }
+      it 'should not import the sales correctly' do
+        expect(flash[:notice]).to be_eql('As importação de vendas foi mal-sucedida.')
+      end
 
-      expect(Sale.count).to eq 4
-    end
-
-    it 'should import the sales correctly' do
-      file = fixture_file_upload('dados_invalidos.txt', 'text/txt')
-
-      post :create, params: { file: file }
-
-      expect(flash[:notice]).to be_eql('As importação de vendas foi mal-sucedida.')
-    end
-
-    it 'should not increase the count of Sales when file is empty' do
-      file = fixture_file_upload('dados_invalidos.txt', 'text/txt')
-
-      post :create, params: { file: file }
-
-      expect(Sale.count).to eq 0
+      it 'should not increase the count of Sales' do
+        expect(Sale.count).to eq 0
+      end
     end
   end
 end
